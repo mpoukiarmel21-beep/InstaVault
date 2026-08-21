05dd676b80f7584da36ae8b1148948af1697c905
mpoukiarmel21-beep <mpoukiarmel21@gmail.com>
2026-08-21 10:26:20 +0100

    Fix IVFloatingButton: remove Swift syntax, fix property types, add ThemeManager import

--- Tweak/Source/IVFloatingButton.h
+++ Tweak/Source/IVFloatingButton.h
@@ -1,8 +1,7 @@
 #import <UIKit/UIKit.h>
 @interface IVFloatingButton : UIButton
-@property (nonatomic, strong) UILabel *badge;
 @property (nonatomic, copy) void (^onTap)(void);
-- (void)setCount:(NSInteger)c;
+- (void)setCount:(NSUInteger)c;
 - (void)setHex:(NSString *)hex;
 - (void)attachToView:(UIView *)v;
 @end
--- Tweak/Source/IVFloatingButton.m
+++ Tweak/Source/IVFloatingButton.m
@@ -1,9 +1,9 @@
 #import "IVFloatingButton.h"
+#import "IVThemeManager.h"
 
 @implementation IVFloatingButton {
-    UIView *_badge;
-    UILabel *_count;
-    UIColor *_currentHex;
+    UIView *_badgeView;
+    UILabel *_countLabel;
     CGPoint _initialCenter;
 }
 
@@ -11,14 +11,13 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor systemBlueColor];
-        self.layer.cornerRadius = frame.size.height/2;
+        self.layer.cornerRadius = frame.size.height / 2;
         self.layer.shadowColor = [UIColor blackColor].CGColor;
         self.layer.shadowOpacity = 0.25;
         self.layer.shadowRadius = 8;
         self.layer.shadowOffset = CGSizeMake(0, 4);
         self.clipsToBounds = NO;
 
-        // Icon
         UIImageView *icon = [[UIImageView alloc] initWithImage:[UIImage systemImageNamed:@"square.stack.3d.up.fill"]];
         icon.tintColor = [UIColor whiteColor];
         icon.contentMode = UIViewContentModeScaleAspectFit;
@@ -31,40 +30,36 @@
             [icon.heightAnchor constraintEqualToConstant:28]
         ]];
 
-        // Badge
-        _badge = [UIView new];
-        _badge.backgroundColor = [UIColor systemRedColor];
-        _badge.layer.cornerRadius = 10;
-        _badge.clipsToBounds = YES;
-        _badge.hidden = YES;
-        _badge.translatesAutoresizingMaskIntoConstraints = NO;
-        [self addSubview:_badge];
+        _badgeView = [UIView new];
+        _badgeView.backgroundColor = [UIColor systemRedColor];
+        _badgeView.layer.cornerRadius = 10;
+        _badgeView.clipsToBounds = YES;
+        _badgeView.hidden = YES;
+        _badgeView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:_badgeView];
 
-        _count = [UILabel new];
-        _count.textColor = [UIColor whiteColor];
-        _count.font = [UIFont systemFontOfSize:10 weight:UIFontWeightBold];
-        _count.textAlignment = NSTextAlignmentCenter;
-        _count.translatesAutoresizingMaskIntoConstraints = NO;
-        [_badge addSubview:_count];
+        _countLabel = [UILabel new];
+        _countLabel.textColor = [UIColor whiteColor];
+        _countLabel.font = [UIFont systemFontOfSize:10 weight:UIFontWeightBold];
+        _countLabel.textAlignment = NSTextAlignmentCenter;
+        _countLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        [_badgeView addSubview:_countLabel];
 
         [NSLayoutConstraint activateConstraints:@[
-            [_badge.topAnchor constraintEqualToAnchor:self.topAnchor constant:-4],
-            [_badge.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:4],
-            [_badge.heightAnchor constraintEqualToConstant:20],
-            [_badge.widthAnchor constraintGreaterThanOrEqualToConstant:20],
-            [_count.centerXAnchor constraintEqualToAnchor:_badge.centerXAnchor],
-            [_count.centerYAnchor constraintEqualToAnchor:_badge.centerYAnchor],
-            [_count.leadingAnchor constraintEqualToAnchor:_badge.leadingAnchor constant:4],
-            [_count.trailingAnchor constraintEqualToAnchor:_badge.trailingAnchor constant:-4]
+            [_badgeView.topAnchor constraintEqualToAnchor:self.topAnchor constant:-4],
+            [_badgeView.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:4],
+            [_badgeView.heightAnchor constraintEqualToConstant:20],
+            [_badgeView.widthAnchor constraintGreaterThanOrEqualToConstant:20],
+            [_countLabel.centerXAnchor constraintEqualToAnchor:_badgeView.centerXAnchor],
+            [_countLabel.centerYAnchor constraintEqualToAnchor:_badgeView.centerYAnchor],
+            [_countLabel.leadingAnchor constraintEqualToAnchor:_badgeView.leadingAnchor constant:4],
+            [_countLabel.trailingAnchor constraintEqualToAnchor:_badgeView.trailingAnchor constant:-4]
         ]];
 
-        // Pan gesture for dragging
         UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panned:)];
         [self addGestureRecognizer:pan];
 
-        // Tap gesture (separate from pan)
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped:)];
-        tap.requireGestureRecognizerToFail = pan;
         [self addGestureRecognizer:tap];
     }
     return self;
@@ -76,19 +71,20 @@
     CGPoint t = [g translationInView:host];
     if (g.state == UIGestureRecognizerStateBegan) {
         _initialCenter = self.center;
-        UIView.animateWithDuration:0.15 animations:^{ self.transform = CGAffineTransformMakeScale(1.15, 1.15); };
+        [UIView animateWithDuration:0.15 animations:^{
+            self.transform = CGAffineTransformMakeScale(1.15, 1.15);
+        }];
     } else if (g.state == UIGestureRecognizerStateChanged) {
         self.center = CGPointMake(_initialCenter.x + t.x, _initialCenter.y + t.y);
     } else {
-        UIView.animateWithDuration:0.25 delay:0 usingSpringWithDamping:0.7 initialSpringVelocity:0.5 options:0 animations:^{
+        [UIView animateWithDuration:0.25 delay:0 usingSpringWithDamping:0.7 initialSpringVelocity:0.5 options:0 animations:^{
             self.transform = CGAffineTransformIdentity;
         } completion:nil];
-        // Snap to nearest edge
         CGFloat margin = 20;
         CGRect bounds = host.bounds;
-        CGFloat halfW = self.bounds.size.width/2;
-        CGFloat halfH = self.bounds.size.height/2;
-        CGFloat newX = self.center.x < bounds.size.width/2 ? margin + halfW : bounds.size.width - margin - halfW;
+        CGFloat halfW = self.bounds.size.width / 2;
+        CGFloat halfH = self.bounds.size.height / 2;
+        CGFloat newX = self.center.x < bounds.size.width / 2 ? margin + halfW : bounds.size.width - margin - halfW;
         CGFloat newY = fmax(margin + halfH, fmin(bounds.size.height - margin - halfH, self.center.y));
         [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0.5 options:0 animations:^{
             self.center = CGPointMake(newX, newY);
@@ -101,31 +97,28 @@
 }
 
 - (void)setCount:(NSUInteger)n {
-    if (n == 0) { _badge.hidden = YES; return; }
-    _badge.hidden = NO;
-    _count.text = n > 99 ? @"99+" : [NSString stringWithFormat:@"%lu", (unsigned long)n];
+    if (n == 0) { _badgeView.hidden = YES; return; }
+    _badgeView.hidden = NO;
+    _countLabel.text = n > 99 ? @"99+" : [NSString stringWithFormat:@"%lu", (unsigned long)n];
     [self layoutIfNeeded];
 }
 
 - (void)setHex:(NSString *)hex {
     if (!hex) return;
-    _currentHex = [IVThemeManager shared].hex(hex);
-    self.backgroundColor = _currentHex;
+    self.backgroundColor = [[IVThemeManager shared] hex:hex];
 }
 
 - (void)attachToView:(UIView *)v {
     if (self.superview) [self removeFromSuperview];
     [v addSubview:self];
     self.translatesAutoresizingMaskIntoConstraints = NO;
-    NSLayoutConstraint *trailing = [self.trailingAnchor constraintEqualToAnchor:v.trailingAnchor constant:-20];
-    NSLayoutConstraint *top = [self.topAnchor constraintEqualToAnchor:v.safeAreaLayoutGuide.topAnchor constant:100];
     [NSLayoutConstraint activateConstraints:@[
         [self.widthAnchor constraintEqualToConstant:60],
         [self.heightAnchor constraintEqualToConstant:60],
-        trailing, top
+        [self.trailingAnchor constraintEqualToAnchor:v.trailingAnchor constant:-20],
+        [self.topAnchor constraintEqualToAnchor:v.safeAreaLayoutGuide.topAnchor constant:100]
     ]];
     [self layoutIfNeeded];
-    // Animate in
     self.transform = CGAffineTransformMakeScale(0.1, 0.1);
     self.alpha = 0;
     [UIView animateWithDuration:0.4 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.8 options:0 animations:^{
